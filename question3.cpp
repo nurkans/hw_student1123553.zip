c6ca30a733724a33574b7032a46c92990c383efa
nurkans <102859716+nurkans@users.noreply.github.com>
2024-12-15 19:22:47 +0800

    Update question3.cpp

--- question3.cpp
+++ question3.cpp
@@ -1,69 +1,35 @@
 // question3.cpp
-// Name: [Your Name]
-// Student ID: [Your Student ID]
-// Date of Submission: 28-Nov-2024
+// Name: [Nurbyergyen]
+// Student ID: [1123553]
+// Date of Submission: [2024.12.12]
 
 #include <iostream>
-#include <queue>
 #include <vector>
 using namespace std;
 
-struct Element {
-    int value;
-    int arrayIndex;
-    int elementIndex;
+void dfsHelper(int node, const vector<vector<int>>& adj, vector<int>& visited, vector<int>& result) {
+    visited[node] = 1;
+    result.push_back(node);
 
-    bool operator>(const Element& other) const {
-        return value > other.value;
-    }
-};
-
-vector<int> mergeKSortedArrays(vector<vector<int>>& arrays) {
-    priority_queue<Element, vector<Element>, greater<Element>> minHeap;
-    vector<int> result;
-
-    // Insert the first element of each array into the heap
-    for (int i = 0; i < arrays.size(); i++) {
-        if (!arrays[i].empty()) {
-            minHeap.push({arrays[i][0], i, 0});
+    for (int neighbor : adj[node]) {
+        if (!visited[neighbor]) {
+            dfsHelper(neighbor, adj, visited, result);
         }
     }
+}
 
-    // Extract elements from the heap and insert the next elements
-    while (!minHeap.empty()) {
-        Element current = minHeap.top();
-        minHeap.pop();
-        result.push_back(current.value);
-
-        // Insert the next element from the same array
-        if (current.elementIndex + 1 < arrays[current.arrayIndex].size()) {
-            minHeap.push({arrays[current.arrayIndex][current.elementIndex + 1], current.arrayIndex, current.elementIndex + 1});
-        }
-    }
-
+vector<int> dfsTraversal(const vector<vector<int>>& adj) {
+    vector<int> visited(adj.size(), 0), result;
+    dfsHelper(0, adj, visited, result);
     return result;
 }
 
 int main() {
-    int k;
-    cout << "Enter the number of arrays: ";
-    cin >> k;
-    vector<vector<int>> arrays(k);
+    vector<vector<int>> adj = {{1, 2}, {0, 2}, {0, 1, 3, 4}, {2}, {2}};
+    vector<int> dfs = dfsTraversal(adj);
 
-    for (int i = 0; i < k; i++) {
-        cout << "Enter elements of array " << i + 1 << " (space-separated, end with newline): ";
-        int num;
-        while (cin.peek() != '\n') {
-            cin >> num;
-            arrays[i].push_back(num);
-        }
-        cin.ignore();
-    }
-
-    vector<int> result = mergeKSortedArrays(arrays);
-    cout << "Merged Array: ";
-    for (int num : result) {
-        cout << num << " ";
+    for (int node : dfs) {
+        cout << node << " ";
     }
     cout << endl;
 
